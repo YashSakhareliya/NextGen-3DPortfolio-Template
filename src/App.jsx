17c2ff2130010a3144360dc3ea6ba89fabcc5c10
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-01 23:33:39 +0530

    remove nested file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter } from "react-router-dom"
+import {Navbar, Hero} from './components'
 
 function App() {
   
 
   return (
-    <> </>
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center ">
+        <Navbar />
+        <Hero />
+      </div>
+
+      
+    </div>
+    </BrowserRouter>
   )
 }
 
