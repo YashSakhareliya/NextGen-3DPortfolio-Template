c40987ea66e1993adcf2fbc918c7f8f990924d79
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-02 00:49:16 +0530

    tailwind depedency added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,25 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 import {styles} from '../styles'
+import {logo, menu, close} from '../assets'
 
 const Navbar = () => {
+  const [active, setActive] = useState("")
+
   return (
-    <div>Navbar</div>
+    <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+      <Link 
+      to="/" 
+      className="flex items-center"
+      onClick={() => {
+        setActive("")
+        window.scrollTo(0, 0);
+      }}
+      >
+        <img src={logo} alt="logo" className="w-9 h-9" />
+      </Link>
+
+    </div>
   )
 }
 
