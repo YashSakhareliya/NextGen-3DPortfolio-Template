3126128d27b9112e7c4471205a5ed10d03135d57
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-02 16:22:14 +0530

    add 3D model props

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {Suspense, useState, useEffect} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
 
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+  
   return (
-    <div>Computers</div>
+    <mesh>
+
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+
+  return (
+    <Canvas
+    frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas
