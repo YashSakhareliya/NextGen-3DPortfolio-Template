7741671c808bf27d3a199b6a149069e3be83af3d
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-03-04 16:39:21 +0530

    Project Card Image DOne

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -16,7 +16,27 @@ const ProjectCard = ({
   image,
   source_code_link,
 }) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+      >
+        <div className='relative w-full h-[230px]'>
+        <img
+            src={image}
+            alt='project_image'
+            className='w-full h-full object-cover rounded-2xl'
+          />
 
+        </div>
+      </Tilt>
+    </motion.div>  
+  )
 }
 
 const Works = () => {
